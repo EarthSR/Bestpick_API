d0baf7033ee3201ec53c730f658c0538c8a02ccc
EarthSR <earthsrichok31@gmail.com>
2024-10-28 15:40:57 +0700

    update

--- botgetprice.py
+++ botgetprice.py
@@ -417,7 +417,7 @@ def recommend():
 
 if __name__ == '__main__':
     try:
-        app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5005)
 
     finally:
         driver.quit()
